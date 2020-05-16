804e8f133ff307afa8b05f5ae2248bd730b560cc
zhangliping7394 <18701122267@163.com>
2020-05-16 17:02:15 +0800

    添加map组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,48 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    data: {
+      markers: [{
+        iconPath: "/resources/others.png",
+        id: 0,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        width: 50,
+        height: 50
+      }],
+      polyline: [{
+        points: [{
+          longitude: 113.3245211,
+          latitude: 23.10229
+        }, {
+          longitude: 113.324520,
+          latitude: 23.21229
+        }],
+        color:"#FF0000DD",
+        width: 2,
+        dottedLine: true
+      }],
+      controls: [{
+        id: 1,
+        iconPath: '/resources/location.png',
+        position: {
+          left: 0,
+          top: 300 - 50,
+          width: 50,
+          height: 50
+        },
+        clickable: true
+      }]
+    },
+    regionchange(e) {
+      console.log(e.type)
+    },
+    markertap(e) {
+      console.log(e.detail.markerId)
+    },
+    controltap(e) {
+      console.log(e.detail.controlId)
+    }
   },
   //事件处理函数
   bindViewTap: function() {
